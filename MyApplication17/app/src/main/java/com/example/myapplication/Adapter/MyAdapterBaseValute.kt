495c3433cc0eac4906203038c42547b36a705d50
veednak <44851565+veednak@users.noreply.github.com>
2021-02-19 15:27:08 +0300

    й
    й

---
+++ MyApplication17/app/src/main/java/com/example/myapplication/Adapter/MyAdapterBaseValute.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.myapplication.R
+import java.util.*
+
+class MyAdapterBaseValute(var mCtx: Context, var resources:Int, var items: ArrayList<ModelBaseValute>): ArrayAdapter<ModelBaseValute>(mCtx,resources,items)
+{
+    override fun getView(position:Int, coverterView: View?, parent: ViewGroup): View
+    {
+        val layoutInflater: LayoutInflater = LayoutInflater.from(mCtx)
+        val view: View =layoutInflater.inflate(resources,null)
+
+        val img: ImageView =view.findViewById<ImageView>(R.id.image)
+        val nameB: TextView = view.findViewById<TextView>(R.id.textView1)
+
+
+        nameB.text= items[position].NameB
+        img.setImageDrawable(mCtx.resources.getDrawable(items[position].ImgB))
+        return view
+    }
+}
---
+++ MyApplication17/app/src/main/java/com/example/myapplication/BlrPars/RetrofitInterface.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.BlrPars
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface RetrofitInterface {
+    @get:GET("api/exrates/rates?periodicity=0")
+    val posts: Call<List<Post?>?>?
+    companion object {
+        const val BASE_URL = "https://www.nbrb.by"
+    }
+}
